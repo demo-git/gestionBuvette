6c06d310efc1d818a0178f152af342c45651323a
jeremy <a>
2016-04-29 16:16:13 +0200

    mise en place jquery et bootstrap

--- src/BuvetteBundle/Controller/MainController.php
+++ src/BuvetteBundle/Controller/MainController.php
@@ -6,8 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
+    /**
+     * @Route("/", name="general_accueil")
+     */
     public function indexAction()
     {
-        return $this->render('BuvetteBundle:Default:index.html.twig');
+        return $this->render('BuvetteBundle:Main:index.html.twig');
     }
 }
--- src/CuisineBundle/Controller/MainController.php
+++ src/CuisineBundle/Controller/MainController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
+    /**
+     * @Route("/", name="cuisine_accueil")
+     */
     public function indexAction()
     {
         return $this->render('CuisineBundle:Main:index.html.twig');
